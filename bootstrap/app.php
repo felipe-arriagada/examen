ea5cde76640165539bc9a1708b7d47d07afe9f3f
Felipe Aguilar <felipe.aguilar.arriagada@ciisa.cl>
2025-01-01 01:56:05 -0300

    tercer resplado

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,7 +12,8 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->validateCsrfTokens(
-            except: ['/v1/informacion-basica/*']
+            except: ['/v1/informacion-basica/*', '/v1/quienes-somos/*']
+            
         );
     })
     ->withExceptions(function (Exceptions $exceptions) {
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\InformacionBasicaController;
+use App\Http\Controllers\QuienesSomosController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -10,8 +11,9 @@ Route::get('/', function () {
 Route::get('/v1/informacion-basica', [InformacionBasicaController::class, 'index']);
 Route::put('/v1/informacion-basica/{id}', [InformacionBasicaController::class, 'update']);
 Route::delete('/v1/informacion-basica/{id}', [InformacionBasicaController::class, 'delete']);
-
-
-
 Route::patch('v1/informacion-basica/{id}', [InformacionBasicaController::class, 'patch']);
 
+
+Route::get('v1/quienes-somos', [QuienesSomosController::class, 'index']);
+Route::put('v1/quienes-somos/{id}', [QuienesSomosController::class, 'update']);
+Route::patch('v1/quienes-somos/{id}', [QuienesSomosController::class, 'update']);
