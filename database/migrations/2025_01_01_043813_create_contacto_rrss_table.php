354d2741514e0eb2e4f9a7cbe69894776d8a56f3
Felipe Aguilar <felipe.aguilar.arriagada@ciisa.cl>
2025-01-01 01:58:33 -0300

    se agregan campos a migration creato contacto_rrss

--- database/migrations/2025_01_01_043813_create_contacto_rrss_table.php
+++ database/migrations/2025_01_01_043813_create_contacto_rrss_table.php
@@ -14,6 +14,9 @@ return new class extends Migration
         Schema::create('contacto_rrss', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->text('instagram')->nullable();
+            $table->string('telefono')->nullable();
+
         });
     }
 
